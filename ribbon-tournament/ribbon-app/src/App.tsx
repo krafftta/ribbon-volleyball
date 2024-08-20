e1a383f9d690722d6374272ab84f7a37affccb3b
Tamara Krafft <tamara.krafft@uni-a.de>
2024-08-20 12:24:26 +0200

    init react app

---
+++ ribbon-tournament/ribbon-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Match, Team, Participant } from '../../ribbonUtils'
+import { Scheduler } from '../../scheduler'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span className='heading'> Schleifchenturnier </span>
+    </div>
+  );
+}
+
+export default App;
