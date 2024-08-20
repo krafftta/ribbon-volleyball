8aab0d03c21a7c78d2990c4ca7a87b68b475c129
Tamara Krafft <tamara.krafft@uni-a.de>
2024-08-20 13:16:24 +0200

    add participant via ui

--- ribbon-tournament/ribbon-app/src/App.tsx
+++ ribbon-tournament/ribbon-app/src/App.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import './App.css';
-import { Match, Team, Participant } from '../../ribbonUtils'
-import { Scheduler } from '../../scheduler'
+import { Match, Team, Participant } from './models/ribbonUtils'
+import { Scheduler } from './models/scheduler'
 import InputField from './components/InputField';
+import { useState } from 'react';
+import { scheduler } from 'timers/promises';
 
 const App: React.FC = () => {
+
+  const [participant,setParticipant] = useState<string>("");
+  const [participants, setParticipants] = useState<Participant[]>([]);
+  let scheduler = new Scheduler(participants)
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (participant) {
+      setParticipants([...participants, new Participant(participant, [2])])
+      let p = new Participant(participant, [2])
+      scheduler.addParticipant(p)
+      setParticipant("")
+    }
+
+  };
+
+ 
+  
+
   return (
     <div className="App">
       <span className='heading'> Schleifchenturnier </span>
-      <InputField />
+      <InputField participant={participant} setParticipant={setParticipant} handleAdd={handleAdd}/>
+      {participants.map((p) => (
+        <li> {p.name}</li>))
+        }
     </div>
+
   );
 }
 
--- ribbon-tournament/ribbon-app/src/components/InputField.tsx
+++ ribbon-tournament/ribbon-app/src/components/InputField.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './styles.css'
 
-const InputField = () => {
+interface Props{
+  participant: string;
+  setParticipant: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd:(event:React.FormEvent)=> void;
+}
+
+const InputField: React.FC<Props> = ({participant, setParticipant, handleAdd}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-   <form className='input'>
-        <input type='input' placeholder='Gebe einen Teilnehmernamen an' className='input__box'></input>
+   <form className='input' onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur()
+    }}>
+        <input type='input'  
+        ref = {inputRef}
+        value={participant} 
+        onChange={(event)=>setParticipant(event.target.value)}
+        placeholder='Gebe einen Teilnehmernamen an' className='input__box'>
+        </input>
         <button className='input_submit' type='submit'> 
             + 
         </button>
